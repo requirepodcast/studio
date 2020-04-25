67ef76f2bcf1375ad66d40c8621818853cca2e71
AdamSiekierski <adam.siekiera@outlook.com>
2020-04-25 19:54:13 +0200

    Basic version of rendering algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import ffmpeg from 'fluent-ffmpeg';
+import { wordWrap } from './utils/wordWrap';
 
-const timeline = ffmpeg()
+const episodeTitle = 'require(#6) - Ciągła Integracja';
+const audioFile = 'assets/linus_music.mp3';
+
+const timeline = ffmpeg(audioFile)
   .input('assets/animation.mp4')
+  .inputOption('-stream_loop -1')
   .input('assets/RequireLogo.png')
   .complexFilter([
-    '[1:v]scale=600:600 [logo]',
-    '[0][logo]overlay=180:240 [combined]',
-    "[combined]drawtext=fontfile=assets/FiraCode.ttf:text='dupa':fontsize=80:fontcolor=white:x=w/2:y=(h-text_h)/2",
-  ]);
+    '[2:v]scale=600:600 [logo]',
+    '[1][logo]overlay=180:240 [combined]',
+    `[combined]drawtext=fontfile=assets/FiraCode.ttf:text='${wordWrap(
+      episodeTitle,
+    )}':fontsize=80:fontcolor='#ff5370':x=840:y=(h-text_h)/2`,
+  ])
+  .outputOption('-shortest');
 
 const output = timeline.output('output/output.mp4');
 
---
+++ src/utils/wordWrap.ts
@@ -0,0 +1,17 @@
+export function wordWrap(text: string) {
+  const words = text.split(' ');
+  let letters = 0;
+  let wordWrappedText = '';
+
+  for (let word of words) {
+    letters += word.length + 1;
+    if (letters > 22) {
+      letters = 0;
+      wordWrappedText += `\n${word}`;
+    } else {
+      wordWrappedText += ` ${word}`;
+    }
+  }
+
+  return wordWrappedText;
+}
