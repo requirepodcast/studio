29e5753c2685b0a1f757efc7906c9e35e7f41b86
AdamSiekierski <adam.siekiera@outlook.com>
2020-04-27 15:16:48 +0200

    Main menu

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -3,6 +3,9 @@ import '../css/index.scss';
 import '@material/mwc-top-app-bar';
 import '@material/mwc-icon-button';
 import '@material/mwc-drawer';
+import '@material/mwc-list/mwc-list';
+import '@material/mwc-list/mwc-list-item';
+import '@material/mwc-icon';
 
 document.querySelector('#navigationButton').addEventListener('click', () => {
   document.querySelector('mwc-drawer').open = !document.querySelector('mwc-drawer').open;
--- src/routes/api.router.ts
+++ src/routes/api.router.ts
@@ -2,16 +2,10 @@ import express, { Request, Response } from 'express';
 import multer from 'multer';
 import { protectedApiRoute } from '../utils/auth';
 
-const upload = multer({ dest: '../public/uploads' });
 const router = express.Router();
 
 router.get('/', protectedApiRoute, (req, res) =>
   res.json({ message: `requirepodcast's render server api` }),
 );
-
-router.post('/audio/upload', protectedApiRoute, upload.single('audioFile'), (req, res) => {
-  console.log(req.file);
-  return res.json({ message: 'kul' });
-});
 
 export default router;
