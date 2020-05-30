b68fd7b3ed212c245275fe1534e7dbbe8f3155b0
Adam Siekierski <adam.siekiera@outlook.com>
2020-05-30 12:05:22 +0200

    Adjust the nginx and certbot for new domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(render-server.podcast.gq)
+domains=(studio.podcast.gq)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="require@podcast.gq" # Adding a valid address is strongly recommended
