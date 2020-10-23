6c3388932c1795f4d787458743ec9577f9aa2f85
Adam Siekierski <a@siekierski.ml>
2020-10-23 21:38:39 +0200

    Basic table showing currently created links

--- src/app/js/shortener.js
+++ src/app/js/shortener.js
@@ -3,3 +3,38 @@ import '../styles/shortener.scss'
 
 import '@material/mwc-textfield';
 import '@material/mwc-button';
+
+function getLinks() {
+  import('axios').then(({ default: axios }) => {
+    axios.get('/api/v1/shortener').then(({data}) => {
+      const dataTable = document.querySelector('.mdc-data-table__content');
+
+      dataTable.innerHTML = '';
+
+      for (const link of data) {
+        const row = document.createElement('tr');
+        row.setAttribute('class', 'mdc-data-table__row');
+
+        const linkSlug = document.createElement('td');
+        linkSlug.setAttribute('class', 'mdc-data-table__cell');
+        linkSlug.textContent = link.slug;
+
+        const linkDest = document.createElement('td');
+        linkDest.setAttribute('class', 'mdc-data-table__cell');
+
+        const linkDestLink = document.createElement('a')
+        linkDestLink.setAttribute('href', link.destination)
+        linkDestLink.textContent = link.destination;
+
+        linkDest.appendChild(linkDestLink)
+
+        row.appendChild(linkSlug);
+        row.appendChild(linkDest);
+
+        dataTable.appendChild(row);
+      }
+    })
+  })
+}
+
+getLinks()
